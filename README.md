f6a92651ad32564e8764af43f787a27a2afed808
Stephen Dolan <stephen@neadwerx.com>
2020-04-10 17:30:31 -0400

    Initial commit

--- README.md
+++ README.md
@@ -2,12 +2,13 @@
 
 1. [Install the CLASP command line tool](https://github.com/google/clasp/#install)
 1. Run `clasp login` to get your Google credentials set up
-1. Create the project from `clasp` in your [Google Scripts](script.google.com) using `clasp create gas-calendar-note-builder`.
-1. Update the following user properties appropriately:
-   - CONFLUENCE_API_USER
-   - CONFLUENCE_API_TOKEN
-   - CONFLUENCE_MEETING_NOTE_TEMPLATE_ID
-   - CONFLUENCE_PARENT_PAGE_ID
-1. Set up an [Apps Script schedule](https://developers.google.com/apps-script/guides/triggers/installable#time-driven_triggers)
-1. ???
-1. Profit
+1. Create the project from `clasp` in your [Google Scripts](script.google.com) using `clasp create {project-name-that-you-pick`.
+1. Update the following values in the `defineUserProperties` function appropriately:
+   - CONFLUENCE_API_BASE_URL - The base URL of your instance's Confluence API, like `https://yourcompany.atlassian.net/wiki/rest/api`
+   - CONFLUENCE_API_USER - The user to authenticate against the Confluence API, like `you@company.com`
+   - CONFLUENCE_API_TOKEN - The token created in [your Atlassian account](https://id.atlassian.com/manage-profile/security/api-tokens), like `123918yhf2jfga238fa2`
+   - CONFLUENCE_TEMPLATE_ID - The ID of the template to use for meeting notes
+   - CONFLUENCE_PARENT_PAGE_ID - Which page to use as the parent for all meeting notes
+1. Run `clasp push` to push the new user properties you've just defined up to the script
+1. Set up an [Apps Script schedule](https://developers.google.com/apps-script/guides/triggers/installable#time-driven_triggers) for the project you've created.
+1. Enjoy!
--- src/main.ts
+++ src/main.ts
@@ -1,82 +1,151 @@
-class SimpleMessage {
-  subject: string;
-  url: string;
-  body: string;
-
-  constructor(subject: string, id: string, body: string) {
-    this.subject = subject;
-    this.url = this.linkFromId(id);
-    this.body = body;
-  }
-
-  private linkFromId(id: string): string {
-    const baseUrl = "https://mail.google.com/mail/u/0/#all";
-
-    return `${baseUrl}/${id}`;
-  }
+// Expected format from a Confluence Template GET request:
+// https://developer.atlassian.com/cloud/confluence/rest/#api-api-template-contentTemplateId-get
+interface ConfluenceTemplate {
+  templateId: string;
+  editorVersion: string;
+  description: string;
+  templateType: string;
+  name: string;
+  body: {
+    storage: {
+      representation: string;
+      value: string;
+    };
+  };
 }
 
-class TaskEmail {
-  message: SimpleMessage;
-  prefix: string;
-
-  constructor(message: SimpleMessage, prefix: string) {
-    this.message = message;
-    this.prefix = prefix;
-  }
-
-  public send(): boolean {
-    const recipient = this.getRecipient();
-
-    if (recipient === null) return false;
-
-    MailApp.sendEmail(recipient, this.getSubject(), this.getBody());
-
-    return true;
-  }
-
-  private getSubject(): string {
-    const title = `-- ${this.prefix} : ${this.message.subject}`;
-    const dueDate = `# tomorrow`;
-
-    return `${title} ${dueDate}`;
-  }
-
-  private getBody(): string {
-    return `${this.message.url}\n\n${this.message.body}`;
-  }
-
-  private getRecipient(): string | null {
-    return PropertiesService.getUserProperties().getProperty("TO_EMAIL");
-  }
+// Expected format from a Confluence Content GET request:
+// https://developer.atlassian.com/cloud/confluence/rest/#api-api-content-id-get
+interface ConfluencePage {
+  id: string;
+  type: string;
+  title: string;
+  templateType: string;
+  name: string;
+  space: {
+    key: string;
+  };
 }
 
-function defineUserProperties() {
+// Set up the user-specific information referenced throughout the script
+function defineUserProperties(): void {
   const userProperties = PropertiesService.getUserProperties();
-  userProperties.setProperty("TO_EMAIL", "your-task-email@task-manager.com");
+  userProperties.setProperty("CONFLUENCE_API_BASE_URL", "https://yourcompany.atlassian.net/wiki/rest/api");
+  userProperties.setProperty("CONFLUENCE_API_USER", "you@company.com");
+  userProperties.setProperty("CONFLUENCE_API_TOKEN", "1j23k4j289jfk32j");
+  userProperties.setProperty("CONFLUENCE_MEETING_NOTE_TEMPLATE_ID", "1234567899");
+  userProperties.setProperty("CONFLUENCE_PARENT_PAGE_ID", "123091401");
+}
+
+function confluenceAuthorizationHeaders(): GoogleAppsScript.URL_Fetch.HttpHeaders {
+  const userProperties = PropertiesService.getUserProperties();
+  const username = userProperties.getProperty("CONFLUENCE_API_USER");
+  const token = userProperties.getProperty("CONFLUENCE_API_TOKEN");
+
+  return { Authorization: `Basic ${Utilities.base64Encode(`${username}:${token}`)}` };
+}
+
+function confluenceParentPage(): ConfluencePage {
+  const userProperties = PropertiesService.getUserProperties();
+  const baseUrl = userProperties.getProperty("CONFLUENCE_API_BASE_URL");
+  const pageId = userProperties.getProperty("CONFLUENCE_PARENT_PAGE_ID");
+
+  const options = { headers: confluenceAuthorizationHeaders(), contentType: "application/json" };
+  const response = UrlFetchApp.fetch(`${baseUrl}/content/${pageId}`, options);
+
+  return JSON.parse(response.getContentText());
+}
+
+function confluenceTemplate(): ConfluenceTemplate {
+  const userProperties = PropertiesService.getUserProperties();
+  const baseUrl = userProperties.getProperty("CONFLUENCE_API_BASE_URL");
+  const templateId = userProperties.getProperty("CONFLUENCE_MEETING_NOTE_TEMPLATE_ID");
+
+  const options = { headers: confluenceAuthorizationHeaders(), contentType: "application/json" };
+  const response = UrlFetchApp.fetch(`${baseUrl}/template/${templateId}`, options);
+
+  return JSON.parse(response.getContentText());
+}
+
+function createConfluenceNote(event: GoogleAppsScript.Calendar.CalendarEvent): number {
+  const userProperties = PropertiesService.getUserProperties();
+  const baseUrl = userProperties.getProperty("CONFLUENCE_API_BASE_URL");
+  const parentPage = confluenceParentPage();
+
+  const formattedEventDate = event.getStartTime().toLocaleDateString();
+  const params = {
+    title: `${formattedEventDate} -- ${event.getTitle()}`,
+    type: "page",
+    space: { key: parentPage.space.key },
+    ancestors: [{ id: parentPage.id }],
+    body: confluenceTemplate().body,
+  };
+
+  const options = {
+    method: <"post">"post",
+    headers: confluenceAuthorizationHeaders(),
+    contentType: "application/json",
+    payload: JSON.stringify(params),
+  };
+
+  const response = UrlFetchApp.fetch(`${baseUrl}/content`, options);
+  return response.getResponseCode();
+}
+
+// Print out some helpful information regarding how the calendar events are filtered.
+function logRunCriteria(
+  calendar: GoogleAppsScript.Calendar.Calendar,
+  date: Date,
+  statuses: GoogleAppsScript.Calendar.GuestStatus[],
+  includeRecurring: Boolean,
+  includeAllDay: Boolean
+): void {
+  Logger.log("Searching for calendar events matching the following criteria:");
+  Logger.log(`\tCalendar '${calendar.getName()}'`);
+  Logger.log(`\tDate '${date.toLocaleDateString()}'`);
+  Logger.log(`\tStatuses '${statuses.map((status) => status.toString()).join(", ")}'`);
+  Logger.log(`\t${includeRecurring ? "No recurring events" : "Recurring events allowed"}`);
+  Logger.log(`\t${includeAllDay ? "No all day events" : "All day events allowed"}`);
+}
+
+function calendarEvents(): GoogleAppsScript.Calendar.CalendarEvent[] {
+  const calendar = CalendarApp.getDefaultCalendar();
+
+  const today = new Date();
+
+  const validAttendanceStatuses = [
+    CalendarApp.GuestStatus.OWNER,
+    CalendarApp.GuestStatus.MAYBE,
+    CalendarApp.GuestStatus.YES,
+  ];
+
+  const includeRecurring = false;
+  const includeAllDay = false;
+
+  logRunCriteria(calendar, today, validAttendanceStatuses, includeRecurring, includeAllDay);
+
+  let events = calendar
+    .getEventsForDay(today, { statusFilters: validAttendanceStatuses })
+    .filter((event) => event.isRecurringEvent() === includeRecurring)
+    .filter((event) => event.isAllDayEvent() === includeAllDay);
+
+  Logger.log(`Found ${events.length} valid events in the search criteria.`);
+
+  return events;
 }
 
 function main() {
   defineUserProperties();
-  const labelsToProcess = ["@action", "@waiting"];
 
-  labelsToProcess.forEach(labelText => {
-    const label = GmailApp.getUserLabelByName(labelText);
+  calendarEvents().forEach((event) => {
+    Logger.log(`Processing event '${event.getTitle()}'...`);
+    const responseStatusCode = createConfluenceNote(event);
 
-    label.getThreads().forEach(thread => {
-      const latestMessage = thread.getMessages().pop();
+    if (responseStatusCode !== 200) {
+      Logger.log("\tFailed to create Confluence notes for event.");
+      Logger.log(`\tReceived HTTP status code ${responseStatusCode} from Confluence API.`);
+    }
 
-      if (latestMessage === undefined) return;
-
-      const subject = latestMessage.getSubject();
-      const messageId = thread.getId();
-      const body = latestMessage.getPlainBody();
-
-      const message = new SimpleMessage(subject, messageId, body);
-
-      if (new TaskEmail(message, labelText).send()) {
-        thread.removeLabel(label);
-      }
-    });
+    Logger.log("\tSuccessfully created Confluence note for event.");
   });
 }
